e49c5bce9bc76e520323adf1974bf85c9c71f871
Jordan Kuschner <jkusc002@ucr.edu>
2024-05-20 15:33:34 -0700

    Updated latest game retriever

--- commands/get_score.js
+++ commands/get_score.js
@@ -1,3 +1,9 @@
+//Logic Sequence
+// 1. Get TeamID for user inputted team
+// 2. Get most recent game ID for inputted team (TeamGameStatsBySeason)
+// 3. Use most recent game ID to look up box score of most recent game (Box Score)
+// 4. Compose response message based on box score data.
+
 module.exports = {
     name: 'getscore',
     description: 'Gets the score of the most recent game for a specified baseball team',
@@ -8,48 +14,69 @@ module.exports = {
 
         const teamName = args.join(' ');
         const apiKey = '5ef01e673e2c49b48103d2b59d5a4f00';
-        const firstUrl = `https://api.sportsdata.io/v3/mlb/scores/json/AllTeams?key=${apiKey}`;
+        const getTeamIDURL = `https://api.sportsdata.io/v3/mlb/scores/json/AllTeams?key=${apiKey}`;
 
         try {
-            const nameResponse = await fetch(firstUrl);
+            const nameResponse = await fetch(getTeamIDURL);
             if (!nameResponse){
                 throw new Error('First Network response was not ok');
             }
 
             const nameData = await nameResponse.json();
             const id = nameData.find(team => team.Name === teamName);
-            const date = '2024-05-15'
-            const secondUrl = `https://api.sportsdata.io/v3/mlb/scores/json/ScoresBasic/${date}?key=${apiKey}`;
-            const response = await fetch(secondUrl);
-            if(!response.ok) {
+
+            const currSeason = "2024";
+            const getGameIDURL = `https://api.sportsdata.io/v3/mlb/scores/json/TeamGameStatsBySeason/${currSeason}/${id.TeamID}/1?key=${apiKey}`;
+            const getGameResponse = await fetch(getGameIDURL);
+            if(!getGameResponse.ok) {
                 throw new Error('Second Network response was not ok');
             }
 
-            const data = await response.json();
+            const data = await getGameResponse.json();
+           // const game = data.find(game => (game.HomeTeamID === id.TeamID || game.awayTeamID === id.TeamID));
+            const game = data.find(gam => gam.TeamID === id.TeamID );
 
-            const game = data.find(game => (game.HomeTeamID === id.TeamID || game.awayTeamID === id.TeamID));
-            const thirdURL = `https://api.sportsdata.io/v3/mlb/stats/json/BoxScore/${game.GameID}?key=${apiKey}`;
+            const opponentID = game.OpponentID;
+            console.log("OID" + opponentID);
+            const opponent = nameData.find(opp => opp.TeamID === opponentID) ;
+            const opponentName = opponent.Name;
+
+           const thirdURL = `https://api.sportsdata.io/v3/mlb/stats/json/BoxScore/${game.GameID}?key=${apiKey}`;
+
             const gameResponse = await fetch(thirdURL);
-
+            
             if(!gameResponse.ok){
+                
                 throw new Error('Game Network response was not ok');
+                
 
             }
 
-            const gameData = await response.json();
+            const boxScore = await gameResponse.json();
+         //   console.log(gameData);
+           // const boxScore = gameData.find(box => box.GameID === game.GameID);
+            
+            const runsHome = boxScore.Game.HomeTeamRuns;
+            const runsAway = boxScore.Game.AwayTeamRuns;
+            const result = `The most recent game for ${teamName} was on ${boxScore.Game.DateTime.substring(0, 10)} against the ${opponentName}.`;
+            const homeWin = `The ${teamName} beat the ${opponentName} by a score of ${runsHome} - ${runsAway}.`;
+            const awayWin = `The ${teamName} beat the ${opponentName} by a score of ${runsAway} - ${runsHome}.`;
+            const homeLoss = `The ${teamName} lost to the ${opponentName} by a score of ${runsAway} - ${runsHome}.`;
+            const awayLoss = `The ${teamName} lost to the ${opponentName} by a score of ${runsHome} - ${runsAway}.`;
 
-            if(!gameData) {
-                return message.channel.send(`Could not find recent game results for the: ${teamName}`);
-
-            }
-            const result = `The most recent game for ${teamName} was on ${gameData.DateTime} against ${teamGame.Opponent}.`;
-            const win = `The ${teamName} beat the ${teamGame.Opponent} by a score of ${teamGame.Score} - ${teamGame.OpponentScore}.`;
-            const loss = `The ${teamName} lost to the ${teamGame.Opponent} by a score of ${teamGame.OpponentScore} - ${teamGame.Score}.`;
             message.channel.send(result);
-            if(teamGame.Score > teamGame.OpponentScore){
-                message.channel.send(win);
+            if(id.TeamId === boxScore.Game.HomeTeamID){
+                if(runsAway > runsHome){
+                    message.channel.send(homeWin);
+                }else{
+                    message.channel.send(homeLoss);
+                }
             }else{
-                message.channel.send(loss);
+                if(runsAway > runsHome){
+                    message.channel.send(awayWin);
+                }else{
+                    message.channel.send(awayLoss);
+                }
             }
         }catch (error) {
             console.error('Error fetching game data:', error);
